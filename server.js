701a77e21b67aaf5e3ea89f43b6e7a2e34fa19aa
Elliot Voris <elliot@voris.me>
2021-07-16 13:16:40 -0500

    Initial Customization
    Looks like I'll pass all the tests set out by freeCodeCamp. Now, I
    just have to figure out how to transfer it to my dreamhost account.
    
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,9 +19,21 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api", (req, res) => {
+  let nowDate = new Date();
+  res.json({unix: nowDate.getTime(), utc: nowDate.toUTCString()});
+})
+
+app.get("/api/:timestamp", (req, res) => {
+  let reqDate = req.params.timestamp.match(/\D/) ? req.params.timestamp : parseInt(req.params.timestamp);
+  let dateObj = new Date(reqDate);
+  if (dateObj == "Invalid Date") return res.json({error: "Invalid Date"});
+  res.json({unix: dateObj.getTime(), utc: dateObj.toUTCString()});
 });
 
 
